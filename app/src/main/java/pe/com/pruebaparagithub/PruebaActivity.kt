b94d81f474f4c61adc3c731ea94e813ec3466c0a
JaOb <jaimeobispo31@gmail.com>
2020-06-23 00:52:06 -0500

    Initial commit

---
+++ app/src/main/java/pe/com/pruebaparagithub/PruebaActivity.kt
@@ -0,0 +1,18 @@
+package pe.com.pruebaparagithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_prueba.*
+
+class PruebaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prueba)
+
+        btnAceptar.setOnClickListener {
+            txtTexto.text = "Hola Mundo"
+        }
+
+    }
+}
